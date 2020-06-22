9ff03509786677cc72b5e9ce55c0278d47c19554
krosd <krosdmitrij@yandex.ru>
2020-06-22 16:17:23 +0300

    ResultFragment: Смена состояния сканера при дисмиссе

--- app/src/main/java/ru/example/mlbarcodeexample/result/ResultFragment.kt
+++ app/src/main/java/ru/example/mlbarcodeexample/result/ResultFragment.kt
@@ -1,5 +1,6 @@
 package ru.example.mlbarcodeexample.result
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,8 @@ import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.dialog_result.*
 import ru.example.mlbarcodeexample.R
+import ru.example.mlbarcodeexample.Workflow
+import ru.example.mlbarcodeexample.WorkflowState
 
 class ResultFragment: BottomSheetDialogFragment(){
 
@@ -24,6 +27,11 @@ class ResultFragment: BottomSheetDialogFragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         titleTextView.text = "Товар найден"
         subtitleTextView.text = "Код: ${arguments?.getString("barcode")}"
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        (activity as? Workflow)?.onWorkflowStateChange(WorkflowState.DETECTING)
+        super.onDismiss(dialog)
     }
 
 }
